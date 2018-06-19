46aededf44b02cc386e0a44ef7c421bd71d09b0d
Familia Hernandez <Familia Hernandez@Pedro>
2018-06-18 20:27:44 -0600

    ayyyy noo

--- src/hola/Hola.java
+++ src/hola/Hola.java
@@ -19,6 +19,7 @@ public class Hola {
     public static void main(String[] args) {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null, "hola yami");
+         JOptionPane.showMessageDialog(null, "que rayos es esto");
     }
     
 }
